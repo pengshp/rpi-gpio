ecedaf4ec0bf77717c8af5eb519d960cab02a3f6
pengshp <pengshp3@outlook.com>
2016-05-14 16:15:15 +0800

    改变编码为UTF-8

--- 1_on_off.py
+++ 1_on_off.py
@@ -4,19 +4,19 @@ __author__ = 'pengshp'
 
 import RPi.GPIO as GPIO
 import time
-#¿¿¿
-# BOARD±àºÅ·½Ê½£¬»ùÓÚ²å×ùÒı½Å±àºÅ
+
+# è®¾ç½®ç®¡è„šç¼–å·æ¨¡å¼ä¸ºBOARD
 GPIO.setmode(GPIO.BOARD)
 mode = GPIO.getmode()
 
-#¹Ø±Õ¾¯¸æ
+#å…³é—­è­¦å‘Š
 GPIO.setwarnings(False)
 
-#ÉèÖÃ¹Ü½Å
-GPIO.setup(11,GPIO.OUT)#11ÎªÊä³ö
-GPIO.setup(12,GPIO.IN)#12ÎªÊäÈë
+#è®¾ç½®è¾“å…¥å’Œè¾“å‡º
+GPIO.setup(11,GPIO.OUT)#
+GPIO.setup(12,GPIO.IN)#
 
-#³õÊ¼»¯Îª¸ßµçÆ½
+#åˆå§‹åŒ–å¼•è„šä¸ºé«˜ç”µå¹³
 GPIO.setup(channel, GPIO.OUT, initial=GPIO.HIGH)
 
 chan_list = [11,12]
